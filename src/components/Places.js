44b728ff458c7589749b072384a557270c273c8c
Dioni Mercado <me@dionimercado.com>
2018-09-27 10:31:09 -0400

    Now fetching placesNearby using the Google Places API

--- src/components/Places.js
+++ src/components/Places.js
@@ -8,7 +8,9 @@ const Places = ({ data }) => (
       <li key={place.id} className="list-group-item">
         <h4>{place.name}</h4>
         <div>
-          <span className="badge badge-secondary mr-3">{place.rating}</span>
+          <span className="badge badge-secondary mr-3">
+            {(place.rating * 1).toFixed(1)}
+          </span>
           <StarRatings
             rating={place.rating}
             numberOfStars={5}
