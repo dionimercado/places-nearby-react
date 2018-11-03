d41f0e202b32bf620ad125a8022b6c67f12dd345
Dioni Mercado <me@dionimercado.com>
2018-11-03 12:40:01 -0400

    Added places filter

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,18 +3,53 @@ import Places from "./Places";
 import Map from "./Map";
 
 class Main extends Component {
+  state = {
+    places: [],
+    filteredPlaces: [],
+    filter: 0
+  };
+
+  componentWillUpdate = (nextProps, nextState) => {
+    if (nextProps.places !== nextState.places) {
+      this.setState({ places: nextProps.places });
+    }
+  };
+
   linkTo = path => {
     console.log(path);
     this.props.history.push(path);
   };
+
+  onChangeRating = filter => {
+    const places = this.state.places.filter(place => place.rating >= filter);
+
+    console.log("filtered places", places);
+    this.setState({ filter, filteredPlaces: places });
+  };
+
   render() {
     return (
       <div className="d-flex">
         <div className="w-75">
-          <Map places={this.props.places} linkTo={this.linkTo} />
+          <Map
+            places={
+              this.state.filter > 0
+                ? this.state.filteredPlaces
+                : this.state.places
+            }
+            linkTo={this.linkTo}
+          />
         </div>
         <div className="w-25 pt-5">
-          <Places data={this.props.places} />
+          <Places
+            data={
+              this.state.filter > 0
+                ? this.state.filteredPlaces
+                : this.state.places
+            }
+            filter={this.state.filter}
+            onChangeRating={this.onChangeRating}
+          />
         </div>
       </div>
     );
--- src/components/Places.js
+++ src/components/Places.js
@@ -1,43 +1,62 @@
 import React from "react";
 import StarRatings from "react-star-ratings";
 
-const Places = ({ data }) => (
-  <ul
-    className="list-group mt-0 pt-2"
-    style={{ overflow: "scroll", height: "100vh" }}
-  >
-    <li
-      className="list-group-item active rounded-0 position-fixed w-25"
-      style={{
-        right: 0,
-        top: 0
-      }}
+const Places = ({ data, onChangeRating, filter }) => {
+  const places =
+    data.length > 0 ? (
+      data.map(place => (
+        <li key={place.id} className="list-group-item">
+          <h5>{place.name}</h5>
+          <div>
+            <span
+              className="badge badge-secondary position-relative mr-2"
+              style={{ bottom: "-3px" }}
+            >
+              {(place.rating * 1).toFixed(1)}
+            </span>
+            <StarRatings
+              rating={place.rating}
+              numberOfStars={5}
+              starRatedColor="orange"
+              starDimension="20px"
+              starSpacing="3px"
+            />
+          </div>
+        </li>
+      ))
+    ) : (
+      <h2 className="text-center my-5">Nothing found.</h2>
+    );
+  return (
+    <ul
+      className="list-group mt-0 pt-2"
+      style={{ overflow: "scroll", height: "100vh" }}
     >
-      <h4 className="mb-0" style={{ lineHeight: "54px" }}>
-        Nearby Places
-      </h4>
-    </li>
-    {data.map(place => (
-      <li key={place.id} className="list-group-item">
-        <h5>{place.name}</h5>
-        <div>
-          <span
-            className="badge badge-secondary position-relative mr-2"
-            style={{ bottom: "-3px" }}
-          >
-            {(place.rating * 1).toFixed(1)}
-          </span>
+      <li
+        className="list-group-item active rounded-0 position-fixed w-25"
+        style={{
+          right: 0,
+          top: 0
+        }}
+      >
+        <h4
+          className="mb-0 d-flex justify-content-between"
+          style={{ lineHeight: "54px" }}
+        >
+          <span>Nearby Places</span>
           <StarRatings
-            rating={place.rating}
+            rating={filter}
             numberOfStars={5}
+            changeRating={rating => onChangeRating(rating)}
             starRatedColor="orange"
             starDimension="20px"
             starSpacing="3px"
           />
-        </div>
+        </h4>
       </li>
-    ))}
-  </ul>
-);
+      {places}
+    </ul>
+  );
+};
 
 export default Places;
